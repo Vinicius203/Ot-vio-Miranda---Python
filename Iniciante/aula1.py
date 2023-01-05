6e7d12541e856863334aa04d7ce9bbd64778b64c
Vinicius Martins <viniciusmartinsign@gmail.com>
2023-01-05 12:37:37 -0300

    Comentários

---
+++ Iniciante/aula1.py
@@ -0,0 +1,7 @@
+# <- Comentário de 1 Linha
+print(123) # posso comentar na mesma linha que o código
+
+'''
+- Com o uso de docstrings (aspas) posso comentar várias linhas
+- São necessárias três aspas do mesmo tipo
+'''
