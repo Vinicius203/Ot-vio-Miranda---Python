47df86120bcc7193cb0fd67821fdfdfb4da0a155
Vinicius Martins <viniciusmartinsign@gmail.com>
2023-01-05 13:36:58 -0300

    Concatenação e repetição

---
+++ Iniciante/aula10.py
@@ -0,0 +1,7 @@
+concatenacao = 'Vinicius ' + 'Martins'
+print(concatenacao)
+
+a_dez_vezes = 'A' * 10 # printa A dez vezes
+tres_vezes_vinicius = 3 * 'Vinicius' # printa Vinicius 3 vezes
+print(a_dez_vezes)
+print(tres_vezes_vinicius)
