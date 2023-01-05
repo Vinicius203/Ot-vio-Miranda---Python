11047248064b3da9577f006d13d98a7f9249323f
Vinicius Martins <viniciusmartinsign@gmail.com>
2023-01-05 13:24:01 -0300

    PEP8 e atribuições

---
+++ Iniciante/aula7.py
@@ -0,0 +1,11 @@
+# PEP8: iniciar variáveis com letras minúsculas
+# É possível usar números e underline (_)
+# O sinal = atribui um valor para a variável
+
+nome = 'Vinicius'
+nome_completo = 'Vinicius Martins Freire'
+
+idade = 21
+maior_de_idade = idade >= 18
+
+print("É maior?", maior_de_idade)
