78110327bf55e96e42bc3010dee9420997ed064b
Vinicius Martins <viniciusmartinsign@gmail.com>
2023-01-05 12:57:18 -0300

    Int/Float e função Type

---
+++ Iniciante/aula4.py
@@ -0,0 +1,14 @@
+# Tipos de Dados
+
+print(11) # int
+print(-11) # int
+print(0) # int
+
+print(1.1) # float
+print(0.0) # float
+print(-0.5) # float
+
+# A função type mostra o tipo do valor/variável
+print(type(0.0)) # float
+print(type('Vinicius')) # string
+print(type(0)) # int
