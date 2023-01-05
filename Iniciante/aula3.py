fded3a6e97b68df37f4dc9bc76e0c9d144a061f0
Vinicius Martins <viniciusmartinsign@gmail.com>
2023-01-05 12:51:40 -0300

    Print e Strings

---
+++ Iniciante/aula3.py
@@ -0,0 +1,13 @@
+# O python por ter tipagem dinâmica interpreta automaticamente o tipo da variável
+
+print(1234) # variável inteira
+
+# Ao usar aspas, as variáveis são interpretadas como strings
+print('1234')
+print("1234")
+
+# Escape (NÃO UTILIZAR)
+print("Luiz \"Otávio\"")
+
+# Alternativa para o escape é utilizar aspas diferentes
+print('"Vinicius"')
