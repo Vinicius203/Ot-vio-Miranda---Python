075492101e402c73850049276569370c8995161c
Vinicius Martins <viniciusmartinsign@gmail.com>
2023-01-05 13:47:13 -0300

    Exercício IMC

---
+++ Iniciante/aula12.py
@@ -0,0 +1,14 @@
+nome = 'Luiz Otávio'
+altura = 1.80
+peso = 95
+imc = ...
+
+imc = peso / (altura ** 2)
+# Luiz Otávio tem 1.80 de altura, pesa 95 kg e seu IMC é ?
+
+print('Luiz Otávio tem 1.80 de altura, pesa 95kg e seu IMC é', imc)
+
+
+
+
+
