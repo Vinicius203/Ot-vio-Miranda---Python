64d98bc202c4610340b28cbc20799bb5318265ac
Vinicius Martins <viniciusmartinsign@gmail.com>
2023-01-05 13:09:37 -0300

    Typecasting

---
+++ Iniciante/aula6.py
@@ -0,0 +1,6 @@
+# conversão de tipos, coerção
+# type convertion, typecasting, coercion
+# tipos imutáveis e primitivos: str, int, float, bool
+
+x = 1
+print(type(x), type(str(x))) # int e str (foi convertido)
