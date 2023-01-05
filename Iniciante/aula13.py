1cde80dcbb3b83ff4348c4c7aa2d7cdd0c016426
Vinicius Martins <viniciusmartinsign@gmail.com>
2023-01-05 13:47:48 -0300

    f-Strings

---
+++ Iniciante/aula13.py
@@ -0,0 +1,10 @@
+nome = 'Luiz Otávio'
+altura = 1.80
+peso = 95
+imc = ...
+
+imc = peso / (altura ** 2)
+# Luiz Otávio tem 1.80 de altura, pesa 95 kg e seu IMC é ?
+
+# f-string
+print(f'Luiz Otávio tem {altura:.2f}, pesa {peso}kg e seu IMC é {imc:.2f}.')
