f8ec96cb67d8dc66767ae7669a99f44e721ffe30
Vinicius Martins <viniciusmartinsign@gmail.com>
2023-01-05 12:45:08 -0300

    SEP e END no print

---
+++ Iniciante/aula2.py
@@ -0,0 +1,6 @@
+print(12, 34)
+print(12, 34, sep=' - ', end=' #\n')
+
+# O argumento SEP permite a escolha de um separador para o output
+# O argumento end permite a escolha de um final para o output
+
