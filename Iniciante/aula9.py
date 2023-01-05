ae3e837729820150f5d4785c6a34f051b230d052
Vinicius Martins <viniciusmartinsign@gmail.com>
2023-01-05 13:35:01 -0300

    Operadores aritméticos

---
+++ Iniciante/aula9.py
@@ -0,0 +1,17 @@
+# Operadores Aritméticos
+
+'''
++ Adição
+- Subtração
+* Multiplicação
+/ Divisão
+// Divisão Inteira
+** Exponenciação
+% Módulo ou resto da divisão
+'''
+
+print(10 / 3)
+print(10 // 3)
+
+modulo = 30 % 4
+print("Módulo de 30/4:", modulo)
