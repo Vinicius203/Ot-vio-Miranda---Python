5b1a6711d64a096d3eb1fee38ba711ca6b803687
Vinicius Martins <viniciusmartinsign@gmail.com>
2023-01-07 17:00:46 -0300

    Fatiamento de strings

---
+++ Iniciante/aula27.py
@@ -0,0 +1,13 @@
+# Fatiamento de Strings
+# Fatiamento [i:f:p] [::]
+# Obs: a função len retorna o tamanho da str (quantidade de caracteres)
+
+# Olá mundo
+# 012345678
+
+variavel = 'Olá mundo'
+print(len(variavel))
+print(variavel[4:8])
+print(variavel[4:])
+print(variavel[:8])
+print(variavel[0:len(variavel):1])
