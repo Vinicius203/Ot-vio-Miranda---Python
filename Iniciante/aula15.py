7df2f6e27167cb75142b9630a6954a7c671a72ed
Vinicius Martins <viniciusmartinsign@gmail.com>
2023-01-07 00:14:29 -0300

    Input

---
+++ Iniciante/aula15.py
@@ -0,0 +1,11 @@
+# A função input recebe entrada do usuário
+
+nome  = input('Qual o seu nome? ')
+print(f'O seu nome é {nome = }.')
+
+num1 = input('Digite um número: ')
+num2 = input('Digite outro número: ')
+
+r = int(num1) + int(num2)
+
+print(f'A soma dos números é: {r}')
