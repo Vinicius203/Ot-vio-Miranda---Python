fd8a20e73efe09ef546b4671c2d20fcdb9b68893
Vinicius Martins <viniciusmartinsign@gmail.com>
2023-01-07 01:21:56 -0300

    Operadores - IN / NOT IN

---
+++ Iniciante/aula24.py
@@ -0,0 +1,19 @@
+# Operadores IN e NOT IN
+
+nome = 'Vinicius'
+
+# 0 1 2 3 4 5 6 7
+# V i n i c i u s
+# -8-7-6-5-4-3-2-1
+
+print(nome[2]) # n
+print(nome[-6]) # n
+ 
+ # SEMPRE USAR INDICE POSITIVO
+
+print('n' in nome) 
+print('cius' in nome)
+print('z' in nome)
+print (10 * '-')
+print('cius' not in nome)
+print('eoqwejoiqwejq' not in nome)
