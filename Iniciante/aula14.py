f37eeb67d2b52331627dce6a668908b662df09be
Vinicius Martins <viniciusmartinsign@gmail.com>
2023-01-07 00:05:20 -0300

    Strings - Format

--- Iniciante/aula14.py
+++ Iniciante/aula14.py
@@ -1,6 +1,11 @@
-a = 'A'
-b = 'B'
+a = 'AAA'
+b = 'BBBB'
 c = 1.1
-formato = ''
+string = 'a={} b={} c={:.2f}'
+formato = string.format(a, b, c)
 
 print(formato)
+
+# ou, com f-Strings
+
+print(f'A = {a}, B = {b}, C = {c:.2f}')
