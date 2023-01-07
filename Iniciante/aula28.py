8e2d621cf01f7d70b5e24766facc070dd8d5128f
Vinicius Martins <viniciusmartinsign@gmail.com>
2023-01-07 17:27:44 -0300

    Exercício - Manipulação de strings

---
+++ Iniciante/aula28.py
@@ -0,0 +1,36 @@
+'''
+Exercício
+Peça ao usuário para digitar seu nome
+Peça ao usuário para digitar sua idade
+Se nome e idade forem digitados:
+    Exiba:
+        Seu nome é {nome}
+        Seu nome invertido é {nome invertido}
+        Seu nome contém (ou não) espaços
+        Seu nome tem {n} letras
+        A primeira letra do seu nome é {letra}
+        A última letra do seu nome é {letra}
+Se nada for digitado em nome ou idade:
+    Exiba:
+        "Desculpe, você deixou campos vazios."
+'''
+
+nome = input('Entre com o seu nome: ')
+idade = input('Entre com sua idade: ')
+
+if (not nome) or (not idade):
+    print('Você deixou campos vazios.')
+else:
+    print(f'Seu nome é: {nome}')
+    print(f'Seu nome invertido é: {nome[::-1]}')
+
+    if(' ' in nome):
+        print('Seu nome contém espaços.')
+    else:
+        print('Seu nome não contém espaços.')
+    
+    print(f'Seu nome tem {len(nome)} letras.')
+    print(f'A primeira letra do seu nome é: {nome[0]}')
+    # print(f'A última letra do seu nome é: {nome[len(nome)-1]}')
+    print(f'A última letra do seu nome: {nome[-1]}')
+    
