0eb8ebee410305a8b44fc476ccc58a4b4fa8c103
Vinicius Martins <viniciusmartinsign@gmail.com>
2023-01-07 16:39:05 -0300

    Interpolação de Strings - %

---
+++ Iniciante/aula25.py
@@ -0,0 +1,10 @@
+# Interpolação básica de Strings
+# d e i - int
+# s - string
+# x e X - Hexadecimal (ABCDEF0123456789)
+
+nome = 'Luiz'
+preco = 1000.95897643
+variavel = '%s, o preço é R$%.2f.' % (nome, preco)
+print(variavel)
+print('O hexadecimal de %d é %04x.' % (1500, 15))
