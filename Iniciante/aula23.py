9a2f8e4ba304b8da60ba3205f7620b85001ab9ae
Vinicius Martins <viniciusmartinsign@gmail.com>
2023-01-07 01:11:45 -0300

    Operadores lógicos - NOT

---
+++ Iniciante/aula23.py
@@ -0,0 +1,11 @@
+# Usado para inverter expressões
+# not True = False
+# not False = True
+
+senha = input('Senha: ')
+
+if not senha:
+    print("Você não digitou nada.")
+
+print(not 0) # 0 = False, NOT 0 = True
+print(not 1) # 1 = True, NOT 1 = False
