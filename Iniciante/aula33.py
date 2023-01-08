7dbdb705ce8b31f8aabd8948fd794ef9e653eabc
Vinicius Martins <viniciusmartinsign@gmail.com>
2023-01-08 10:36:19 -0300

    Built - In, Métodos de String

---
+++ Iniciante/aula33.py
@@ -0,0 +1,13 @@
+# Imutáveis que vimos: str, int, float, bool
+
+''' 
+NÃO FUNCIONA, O VALOR DA VARiAVEL STRING É IMUTAVEL
+string = 'Luiz Otávio'
+string[2] = 'U'
+print(string[2])
+'''
+
+string = 'luiz Otávio'
+# outra_variavel = f'{string[:2]}U{string[3:]}'
+# print(outra_variavel)
+print(string.capitalize())
