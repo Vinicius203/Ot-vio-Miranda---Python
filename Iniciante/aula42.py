fb83df1333c14f8034008f76ad08dbc62d8d451d
Vinicius Martins <viniciusmartinsign@gmail.com>
2023-01-08 19:33:25 -0300

    Exercício - letra que apareceu mais vezes na frase

---
+++ Iniciante/aula42.py
@@ -0,0 +1,21 @@
+frase = 'aaaabbbcc'.lower()
+
+i = 0
+qtd_apareceu_mais_vezes = 0
+letra_apareceu_mais_vezes = ''
+
+while i < len(frase):
+    aparicao_letras = frase.count(frase[i])
+
+    if frase[i] == ' ':
+        i += 1
+        continue
+
+    if qtd_apareceu_mais_vezes < aparicao_letras:
+        qtd_apareceu_mais_vezes = aparicao_letras
+        letra_apareceu_mais_vezes = frase[i]
+
+    i += 1
+
+    
+print(f'A letra que apareceu mais vezes foi: {letra_apareceu_mais_vezes}, com um total de {qtd_apareceu_mais_vezes} vezes.')
