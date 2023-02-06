cc3aa1600ee5d8c212d1189af8e9a24eb967248e
Vinicius203 <you@example.com>
2023-02-06 18:27:31 -0300

    Listas

---
+++ Iniciante/aula46.py
@@ -0,0 +1,22 @@
+'''
+Listas em Python
+tipo list - Mutável
+suporta vários valores de qualquer tipo
+Conhecimentos reutilizáveis - índices e fatiamento
+Métodos úteis: append, insert, pop, del, clear, extend, +
+'''
+
+# -----  01234
+# -----  -54321
+
+string = 'ABCDE'  # 5 caracteres (len)
+# print(bool([])) # falso, porque é vazio
+# print(lista, type(lista))
+
+lista = [123, True, 'Luiz Otávio', 1.2]
+lista[2] = 'Vinicius Freire'
+print(lista[2])
+
+
+
+
