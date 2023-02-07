af8054f22e781d77f9d968dd910b9d6ff2305479
Vinicius203 <you@example.com>
2023-02-07 10:05:41 -0300

    Tuplas

---
+++ Iniciante/aula52.py
@@ -0,0 +1,16 @@
+'''
+Tupla - lista imutável
+'''
+
+# Lista
+nome_lista = ['Maria', 'Helena', 'Luiz']
+
+# Tupla
+nome_tupla = 'Maria', 'Helena', 'Luiz'
+# ou
+nome_tupla = ('Maria', 'Helena', 'Luiz')
+
+# nome_tupla = list(nome_tupla)
+# nome_lista = tuple(nome_lista)
+
+print(nome_tupla[2])
