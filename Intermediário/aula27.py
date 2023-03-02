299d84a991f5559d35af323eb47cc96aa1a9a16d
Vinicius203 <viniciusmartinsign@gmail.com>
2023-03-01 22:32:21 -0300

    List comprehension - for duplo

---
+++ Intermediário/aula27.py
@@ -0,0 +1,24 @@
+'''
+lista = []
+
+for x in range(3):
+    for y in range(3):
+        lista.append((x,y))
+'''
+# é o mesmo que:
+
+lista = [
+    (x, y)
+    for x in range(3)
+    for y in range(3)
+]
+
+print(lista)
+print()
+
+lista = [
+    [letra for letra in 'Luiz']
+    for x in range(3)
+]
+
+print(lista)
