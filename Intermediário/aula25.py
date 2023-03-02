70a11d4a324175a81d0c8ece8971e72e41303b90
Vinicius203 <viniciusmartinsign@gmail.com>
2023-03-01 22:04:03 -0300

    List comprehension - map

---
+++ Intermediário/aula25.py
@@ -0,0 +1,35 @@
+# List comprehension em Python
+# é uma forma rápida para criar listas
+# a partir de iteráveis.
+
+# lista = []
+
+# for numero in range(10):
+#   lista.append(numero)
+# print(lista)
+
+# O código acima é o mesmo que:
+# usando list comprehension
+
+lista = [
+    numero * 2
+    for numero in range(10)
+]
+# print(lista)
+
+# Mapamento de dados em list comprehension
+
+produtos = [
+    {'nome': 'p1', 'preco': 20, },
+    {'nome': 'p2', 'preco': 10, },
+    {'nome': 'p3', 'preco': 30, },
+]
+
+novos_produtos = [
+    {**produto, 'preco': produto['preco'] * 1.05}
+    if produto['preco'] > 20 else {**produto}
+    for produto in produtos
+]
+
+# print(novos_produtos)
+print(*novos_produtos, sep='\n')
